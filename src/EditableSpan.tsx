be6b9b0c8bed81cf1ee24aad9651366a11ae956c
Michael Dudzik <mkdudik15@gmail.com>
2025-01-10 00:42:19 +0300

    refactoring

--- src/EditableSpan.tsx
+++ src/EditableSpan.tsx
@@ -2,12 +2,20 @@ import React, {ChangeEvent, useState} from "react";
 
 type EditableSpanType = {
     title: string
+    onChange:(newTitle:string)=>void
 }
-export const EditableSpan: React.FC<EditableSpanType> = ({title}) => {
+export const EditableSpan: React.FC<EditableSpanType> = ({title,onChange}) => {
     const [editMode, setEditMode] = useState(false);
-    const [editTitle, setEditTitle] = useState(title);
-    const activateEditModeHandler = () => setEditMode(true);
-    const activateViewModeHandler = () => setEditMode(false);
+    const [editTitle, setEditTitle] = useState("");
+    const activateEditModeHandler = () => {
+        setEditMode(true)
+        setEditTitle(title)
+
+    }
+    const activateViewModeHandler = () => {
+        setEditMode(false)
+        onChange(editTitle)
+    }
     const onEditTitleChangeHandler = (e: ChangeEvent<HTMLInputElement>) => setEditTitle(e.currentTarget.value)
 
     return editMode
